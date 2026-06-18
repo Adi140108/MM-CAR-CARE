3de4bf4205ed1d2a70c8f5e1d6b73dbbdd20d28b
Adi140108 <adi140108@gmail.com>
2026-06-18 23:04:59 +0530

    Configure base path dynamically for local development and GitHub Actions build

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://Adi140108.github.io',
-  base: '/MM-CAR-CARE'
+  base: process.env.GITHUB_ACTIONS ? '/MM-CAR-CARE' : '/'
 });
+
